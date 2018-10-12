a0e921f7a026ac862f3683bc582bc018274b31b2
BuildTools <unconfigured@null.spigotmc.org>
2018-10-11 19:48:25 -0600

    10/11/2018 - Add and test Location, ListItem, and CropData classes

--- CityOfAaron/src/cityofaaron/CityOfAaron.java
+++ CityOfAaron/src/cityofaaron/CityOfAaron.java
@@ -6,6 +6,9 @@
 */
 package cityofaaron;
 
+import model.CropData;
+import model.ListItem;
+import model.Location;
 import model.Player;
 import model.TeamMember;
 import static model.TeamMember.Taylor;
@@ -16,20 +19,42 @@ public class CityOfAaron {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
+// Player class test
         Player playerOne = new Player();
-        
         playerOne.setName("Fred Flintstone");
-        
         String playerOneName = playerOne.getName();
-        
         System.out.println("Player name = " + playerOneName);
         
+// TeamMember class test 
         TeamMember memberOne = Taylor;
-        
         String memberOneName = memberOne.getName();
         String memberOneTitle = memberOne.getTitle();
-        
         System.out.println("Group member name = " + memberOneName + 
                 ", title = " + memberOneTitle);
+        
+// Location class test
+        Location locationOne = new Location();
+        locationOne.setDescription("Placeholder location");
+        String locationOneDesc = locationOne.getDescription();
+        locationOne.setSymbol("###");
+        String locationOneSymbol = locationOne.getSymbol();
+        System.out.println("Location description = " + locationOneDesc +
+                ", symbol = " + locationOneSymbol);
+        
+// ListItem class test
+        ListItem itemOne = new ListItem();
+        itemOne.setName("Placeholder item");
+        String itemOneName = itemOne.getName();
+        itemOne.setNumber(1);
+        int itemOneNumber = itemOne.getNumber();
+        System.out.println("Item name = " + itemOneName + ", number = " +
+                itemOneNumber);
+        
+// CropData class test
+        CropData year = new CropData();
+        year.setYear(1);
+        int yearNumber = year.getYear();
+        System.out.println("Year = " + yearNumber);
     }
 }
---
+++ CityOfAaron/src/model/Location.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.io.Serializable;
+
+public class Location implements Serializable {
+    private String description;
+    private String symbol;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+    
+}
