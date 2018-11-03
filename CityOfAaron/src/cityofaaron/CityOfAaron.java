b8bcc75bc5516fcc0a3ab3cab027f4b1be77cabd
Taylor Mackintosh <mac15028@byui.edu>
2018-11-03 16:11:22 -0600

    11/3/2018 - Add initial view layer functionality

--- CityOfAaron/src/cityofaaron/CityOfAaron.java
+++ CityOfAaron/src/cityofaaron/CityOfAaron.java
@@ -1,60 +1,31 @@
 /*
 * The main() class file for the cityOfAaron project
 * CIT-260
-* Spring 2018
+* Fall 2018
 * Team members: Taylor Mackintosh
 */
 package cityofaaron;
 
-import model.CropData;
-import model.ListItem;
-import model.Location;
-import model.Player;
-import model.TeamMember;
-import static model.TeamMember.Taylor;
+import model.Game;
+import view.*;
 
 public class CityOfAaron {
-
-    /**
-     * @param args the command line arguments
-     */
+    // variable for keeping a reference to the Game object;
+    private static Game Game = null;
+    
+    // main function - entry point for the program
+    // runs the main menu
     public static void main(String[] args) {
-        
-// Player class test
-        Player playerOne = new Player();
-        playerOne.setName("Fred Flintstone");
-        String playerOneName = playerOne.getName();
-        System.out.println("Player name = " + playerOneName);
-        
-// TeamMember class test 
-        TeamMember memberOne = Taylor;
-        String memberOneName = memberOne.getName();
-        String memberOneTitle = memberOne.getTitle();
-        System.out.println("Group member name = " + memberOneName + 
-                ", title = " + memberOneTitle);
-        
-// Location class test
-        Location locationOne = new Location();
-        locationOne.setDescription("Placeholder location");
-        String locationOneDesc = locationOne.getDescription();
-        locationOne.setSymbol("###");
-        String locationOneSymbol = locationOne.getSymbol();
-        System.out.println("Location description = " + locationOneDesc +
-                ", symbol = " + locationOneSymbol);
-        
-// ListItem class test
-        ListItem itemOne = new ListItem();
-        itemOne.setName("Placeholder item");
-        String itemOneName = itemOne.getName();
-        itemOne.setNumber(1);
-        int itemOneNumber = itemOne.getNumber();
-        System.out.println("Item name = " + itemOneName + ", number = " +
-                itemOneNumber);
-        
-// CropData class test
-        CropData year = new CropData();
-        year.setYear(1);
-        int yearNumber = year.getYear();
-        System.out.println("Year = " + yearNumber);
+        MainMenuView mmv = new MainMenuView();
+        mmv.displayMenu();
     }
+
+    public static Game getGame() {
+        return Game;
+    }
+
+    public static void setGame(Game Game) {
+        CityOfAaron.Game = Game;
+    }
+    
 }
